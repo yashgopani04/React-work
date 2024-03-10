cea5e3c91da3c80449aa23341ddd2b305f38264b
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-03-10 22:55:35 +0100

    App JS

---
+++ basics_01/src/App.js
@@ -0,0 +1,16 @@
+import Helloworld from './Helloworld'
+
+function App() {
+
+const language = 'React JS'
+
+  return (
+    <> 
+    <h1>This is {language}</h1>
+    <Helloworld/>
+  </>
+  );
+}
+
+
+export default App;
