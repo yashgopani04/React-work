f759a9357f4357ee6feca78f2b2058c93a0b3df9
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-03-10 22:55:47 +0100

    Example JS

---
+++ basics_01/src/Helloworld.js
@@ -0,0 +1,12 @@
+function Helloworld(){
+return(
+    <>
+    
+    <h3>Hello, Welcome to react Environment</h3>
+    <p>This is very cool Javascript Library and I am enjoying to learn this</p>
+    
+    </>
+)
+}
+
+export default Helloworld;
