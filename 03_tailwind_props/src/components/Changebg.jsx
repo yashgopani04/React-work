2075ce17f5da20f4220757498e5ea7b3db062712
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-03-14 23:41:47 +0100

    tailwind in React

--- 03_tailwind_props/src/components/Changebg.jsx
+++ 03_tailwind_props/src/components/Changebg.jsx
@@ -1,28 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/Card'
-import Changebg from './components/Changebg'
+import React, { useState } from 'react'
 
+function Changebg() {
 
-function App() 
-{
-  const [count, setCount] = useState(0)
-  const [ color, setColor] = useState("olive")
+    const [ color, setColor] = useState("olive")
 
   return (
-    <>
-    {/* <div>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5' >tailwinds</h1>
 
-     <Card username='React Js' brnText = 'Click me'/>
-     <br/>
-     <Card username='Node JS' brnText = 'Read me'/>
-    
-</div> */}
-
-<div className="w-full h-full duration-200" style={{backgroundColor : color}}>
+    <div className="w-full h-full duration-200" style={{backgroundColor : color}}>
     
     <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
 
@@ -36,11 +20,7 @@ function App()
     </div>    
     </div>
     </div>
-
-    {/* <Changebg/> */}
-
-    </>
   )
 }
 
-export default App
+export default Changebg
